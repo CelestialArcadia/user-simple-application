332ca8d9ca23db02b94806c48a280b57859949f8
CelestialArcadia <22876185+CelestialArcadia@users.noreply.github.com>
2021-12-02 02:03:11 +0100

    Added App Component, Service and Module

---
+++ user/src/main/ui/src/app/app.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class AppService {
+
+	constructor(private http: HttpClient) { }
+
+	rootUrl = '/api';
+
+	getUsers() {
+		return this.http.get(this.rootUrl + '/users');
+	}
+
+	addUser(user: any, id: number) {
+		user.id = id;
+		return this.http.post(this.rootUrl + '/user', user);
+	}
+}
